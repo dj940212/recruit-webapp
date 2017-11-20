853e37e1b13dadfb78009c2d6cdfb6780114212a
dj940212 <2902273280@qq.com>
2017-11-20 15:31:26 +0800

    安装项目依赖,配置redux,router等

---
+++ src/container/login/login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logo from '../../component/logo/logo'
+
+class Login extends React.Component{
+	render(){
+		return(
+			<div>
+				<Logo></Logo>
+				<h2>我是等录页</h2>
+			</div>
+		) 
+	}
+}
+
+export default Login
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Redirect,Switch } from 'react-router-dom'
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Login from './container/login/login'
+import Register from './container/register/register'
+
+const store = createStore(reducers, compose(
+	applyMiddleware(thunk),
+	window.devToolsExtends?window.devToolsExtendsion():f=>f
+))
+
+ReactDOM.render(
+	(<Provider store={store}>
+		<BrowserRouter>
+			<div>
+				<Route path="/login" component={Login}></Route>
+				<Route path="/register" component={Register}></Route>
+			</div>
+		</BrowserRouter>
+	</Provider>),
+	document.getElementById('root')
+);
